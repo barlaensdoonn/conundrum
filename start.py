f68345cf815908261912c28489175f90cd8285c5
Unknown <brandon.aleson@gmail.com>
2018-04-29 15:06:17 -0700

    intial commit

---
+++ start.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# send TCP socket message to Blackmagic HyperDeck Studio Mini player
+# to start looping playback of Conundrum (Gary Hill)
+# 4/29/18
+# updated: 4/29/18
+
+from socket import *
+from time import sleep
+
+
+def encode(msg):
+    return '{}\r\n'.format(msg).encode()
+
+
+def client_send(hostport, msg):
+    with socket(AF_INET, SOCK_STREAM) as client:
+        client.connect(hostport)
+        client.sendall(msg)
+        response = client.recv(1024)
+
+        return response.decode().strip()
+
+
+if __name__ == '__main__':
+    # wait 30 seconds to make sure everyone's awake
+    sleep(30)
+
+    # HyperDeck Ethernet Protocol server listens on port 9993
+    hostport = ('192.168.0.166', 9993)
+    response = client_send(hostport, encode('play: loop: true'))
